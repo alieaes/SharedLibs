61c9ef3b67496d21f1429175a4dfba43b01d7dfc
hogyun.yoon <hogyun.yoon@gytni.com>
2022-09-07 10:21:30 +0900

    추가 : XString, 잦은 형변환의 귀찮음을 해소하기 위한 String 자료형 속도 고려되지 않음, 추후 수정 필요

---
+++ String/SharedXString.cpp
@@ -0,0 +1,49 @@
+﻿#include "stdafx.h"
+
+#include "String/SharedXString.h"
+
+#include <Windows.h>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+std::string XString::toString()
+{
+    return Shared::String::ws2s( _str );
+}
+
+std::wstring XString::toWString()
+{
+    return _str;
+}
+
+bool XString::IsEmpty() const
+{
+    return _str.empty();
+}
+
+int XString::size() const
+{
+    return _str.size();
+}
+
+XString XString::substr(int nDst) const
+{
+    return _str.substr( nDst );
+}
+
+XString XString::substr(int nSrc, int nDst) const
+{
+    return _str.substr( nSrc, nDst );
+}
+
+int XString::find_last_of( XString xs ) const
+{
+    return _str.find_last_of( xs );
+}
+
+const wchar_t* XString::c_str() const
+{
+    return _str.c_str();
+}
---
+++ String/SharedXString.h
@@ -0,0 +1,130 @@
+#ifndef __HDR_SHARED_XSTRING__
+#define __HDR_SHARED_XSTRING__
+
+#include <string>
+
+#include "SharedBase.h"
+#include "String/SharedString.h"
+
+#ifdef USING_QT_LIBS
+#include <qstring.h>
+#endif // USING_QT_LIBS
+
+
+class XString
+{
+public:
+    XString()
+    {
+        _str = std::wstring();
+    }
+
+    XString( const char* c )
+    {
+        _str = Shared::String::s2ws( c );
+    }
+
+    XString( const wchar_t* wc )
+    {
+        _str = wc;
+    }
+
+    XString( const std::wstring& ws )
+    {
+        _str = ws;
+    }
+
+    XString( const std::string& s )
+    {
+        _str = std::wstring( s.begin(), s.end() );
+    }
+
+#ifdef USING_QT_LIBS
+    XString( const QString& qs )
+    {
+        _str = qs.toStdWString();
+    }
+#endif // USING_QT_LIBS
+
+    ~XString()
+    {
+    }
+
+    /////////////// XString <- X ///////////////
+    XString operator = ( std::wstring& ws ) // XString xs = std::wstring( L"TEST" );
+    {
+        return XString( ws );
+    }
+
+    XString operator = ( std::string& s ) // XString xs = std::string( "TEST" );
+    {
+        return XString( s );
+    }
+
+    XString operator = ( const char* c ) // XString xs = "TEST";
+    {
+        return XString( c );
+    }
+
+    XString operator = ( const wchar_t* wc ) // XString xs = L"TEST";
+    {
+        return XString( wc );
+    }
+
+#ifdef USING_QT_LIBS
+    XString operator = ( QString& qs ) // XString xs = QString( "TEST" );
+    {
+        return XString( qs );
+    }
+#endif  // USING_QT_LIBS
+    ////////////////////////////////////////////
+
+    /////////////// XString -> X ///////////////
+    operator std::string() const { return Shared::String::ws2s( _str ); }
+    operator std::wstring() const { return _str; }
+
+#ifdef USING_QT_LIBS
+    operator QString() const { return QString::fromStdWString( _str ); }
+#endif  // USING_QT_LIBS
+    ////////////////////////////////////////////
+
+    XString operator+= ( XString& xs )
+    {
+        _str += xs.string();
+        return _str;
+    }
+
+    XString operator +( std::wstring& xs )
+    {
+        _str += xs;
+        return _str;
+    }
+
+    XString operator +( const std::wstring& xs )
+    {
+        _str += xs;
+        return _str;
+    }
+
+    std::string                      toString();
+    std::wstring                     toWString();
+
+    bool                             IsEmpty() const;
+
+    int                              size() const;
+
+    XString                          substr( int nDst ) const;
+    XString                          substr( int nSrc, int nDst ) const;
+
+    int                              find_last_of( XString xs ) const;
+
+    const wchar_t*                   c_str() const;
+
+    std::wstring                     string() { return _str; }
+protected:
+
+private:
+    std::wstring                     _str;
+};
+
+#endif
