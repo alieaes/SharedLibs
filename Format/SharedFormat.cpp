2e173f2f230063238306c7b4afdef9067a844f69
hogyun.yoon <hogyun.yoon@gytni.com>
2022-09-07 10:54:25 +0900

    추가 : Format, Formatter 사용을 위한 파일 추가

---
+++ Format/SharedFormat.cpp
@@ -0,0 +1,19 @@
+﻿#include "stdafx.h"
+
+#include "Format/SharedFormat.h"
+
+#include <Windows.h>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+namespace Shared
+{
+    namespace Format
+    {
+        void SFormat::Format( const std::wstring& sFmt, ... )
+        {
+        }
+    }
+}
