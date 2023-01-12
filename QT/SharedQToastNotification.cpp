bc7f2d4f0ec9e6b62a5e9bd83f552d9cb5a4c3a7
Alieaes <Alieaes54@gmail.com>
2023-01-13 00:50:36 +0900

    수정 : SharedQToastNotification, 수정 진행 중

--- QT/SharedQToastNotification.cpp
+++ QT/SharedQToastNotification.cpp
@@ -14,27 +14,99 @@
 #endif
 
 const QString BTN_CSS = "QPushButton { border: 2px solid rgb(52, 59, 72); border-radius: 5px; background-color: rgb(52, 59, 72); color: white; } QPushButton:hover { background-color: rgb(57, 65, 80); border: 2px solid rgb(61, 70, 86); } QPushButton:pressed { background-color: rgb(35, 40, 49); border: 2px solid rgb(43, 50, 61); }";
+const QString CLOSE_BTN_CSS = "QPushButton { background-color: #3cacc4; border: none;  border-radius: 5px; } QPushButton:hover { background-color: #369bb1; border-style: solid; border-radius: 4px; } QPushButton:pressed { background-color: #308a9d; border-style: solid; border-radius: 4px; }";
+
+cToastNotificationMgr::cToastNotificationMgr( QWidget* parent /*= NULLPTR*/ )
+{
+    _wdgParent = parent;
+    _ToastSize = QSize( 350, 100 );
+    _SpaceSize = QSize( 20, 20 );
+}
+
+cToastNotificationMgr::~cToastNotificationMgr()
+{
+}
+
+QSize cToastNotificationMgr::GetPosition()
+{
+    QSize size;
+    // parent가 없는 경우에는 데스크탑에 띄우도록 함
+    if( _wdgParent == NULLPTR )
+    {
+        auto primayScreen = QGuiApplication::primaryScreen()->availableGeometry();
+        size = QSize( primayScreen.width() - _ToastSize.width() - _SpaceSize.width(), primayScreen.height() - _ToastSize.height() - _SpaceSize.height() );
+    }
+    else
+    {
+        auto parentScreen = _wdgParent->geometry();
+        size = QSize( parentScreen.width() - _ToastSize.width() - _SpaceSize.width(), 
+                      parentScreen.height() - _ToastSize.height() - _SpaceSize.height() );
+    }
+
+    return size;
+}
+
+void cToastNotificationMgr::MoveStack()
+{
+    QSize position = GetPosition();
+    int nHeight = _ToastSize.height() + _SpaceSize.height();
+    if( _vecNotification.size() > 0 )
+    {
+        int nX = 1;
+        for( auto toast : _vecNotification )
+        {
+            toast->MoveToastUI( position.width(), position.height() - nHeight * nX++ );
+        }
+    }
+}
+
+void cToastNotificationMgr::ShowToastMsg( TOAST_ICON icon, TOAST_BUTTON btn, XString sTitle, XString sMsg, int nTimeOutSec /*= 10*/ )
+{
+    cToastNotification* cToast = new cToastNotification( _wdgParent, icon, btn, sTitle, sMsg, nTimeOutSec );
+    cToast->SetToastBoxSize( _ToastSize );
+    cToast->SetSpaceSize( _SpaceSize );
+    cToast->SetToastUI();
+
+    MoveStack();
+
+    _vecNotification.push_front( cToast );
+    cToast->ShowToast();
+}
+
+void cToastNotificationMgr::ShowToastMsg( QString sTitle, QString sMsg )
+{
+    cToastNotification* cToast = new cToastNotification( _wdgParent, ICON_INFO, BTN_NONE, sTitle, sMsg );
+    cToast->SetToastBoxSize( _ToastSize );
+    cToast->SetSpaceSize( _SpaceSize );
+    cToast->SetToastUI();
+
+    MoveStack();
+
+    _vecNotification.push_front( cToast );
+    cToast->ShowToast();
+}
 
 cToastNotification::cToastNotification()
 {
     Init();
-    SetToastUI();
 }
 
-cToastNotification::cToastNotification( QWidget* parent, TOAST_ICON icon, int nTimeOutSec, TOAST_BUTTON btn, XString sMsg )
+cToastNotification::cToastNotification( QWidget* parent, TOAST_ICON icon, TOAST_BUTTON btn, XString sTitle, XString sMsg, int nTimeOutSec /*= 10*/ )
 {
     Init();
 
+    _nTimeOutSec = nTimeOutSec;
     _wdgParent = parent;
     _eIcon = icon;
     _eBtn = btn;
     _sMsg = sMsg;
-
-    SetToastUI();
+    _sTitle = sTitle;
 }
 
 cToastNotification::~cToastNotification()
 {
+    _wdgToastMsg->hide();
+    disconnect( _btnClose, SIGNAL( clicked() ), this, SLOT( Close() ) );
 }
 
 void cToastNotification::Init()
@@ -48,9 +120,6 @@ void cToastNotification::Init()
 
 void cToastNotification::SetToastUI()
 {
-    SetToastBoxSize( 350, 100 );
-    SetSpaceSize( 10, 20 );
-
     _wdgBase = new QWidget();
     _wdgBase->setAttribute( Qt::WA_TranslucentBackground, true );
 
@@ -59,6 +128,7 @@ void cToastNotification::SetToastUI()
     _wdgBase->setMaximumSize( QSize( _nWidth + _spaceSize.width(), _nHeight + _spaceSize.height() ) );
     _wdgBase->setMinimumSize( QSize( _nWidth + _spaceSize.width(), _nHeight + _spaceSize.height() ) );
     _wdgBase->setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::SubWindow );
+    _wdgBase->setWindowOpacity( 0.8 );
 
     _wdgToastMsg = new QWidget( _wdgBase );
 
@@ -67,6 +137,7 @@ void cToastNotification::SetToastUI()
     _wdgToastMsg->setMaximumSize( QSize( _nWidth, _nHeight ) );
     _wdgToastMsg->setMinimumSize( QSize( _nWidth, _nHeight ) );
     _wdgToastMsg->setWindowFlags( Qt::FramelessWindowHint | Qt::WindowStaysOnTopHint | Qt::SubWindow );
+    _wdgToastMsg->setWindowOpacity( 0.3 );
 
     XString sIcon;
     XString sBgColor;
@@ -82,7 +153,7 @@ void cToastNotification::SetToastUI()
 
     // format 에러 처리 필요 XString sStyle = Shared::Format::Format( "QWidget { background-color:{}; border-radius: 3px; }", sBgColor );
 
-    XString sStyle = QString( "QWidget { background-color:%1; border-radius: 3px; }" ).arg( sBgColor );
+    XString sStyle = QString( "QWidget { background-color:%1; border-radius: 7px; }" ).arg( sBgColor );
     _wdgToastMsg->setStyleSheet( sStyle );
 
     QLayout* left = new QHBoxLayout;
@@ -119,10 +190,10 @@ void cToastNotification::SetToastUI()
     frm->setSizePolicy( QSizePolicy::Expanding, QSizePolicy::Fixed );
 
     QLabel* lblTitle = new QLabel;
-    lblTitle->setText( "<b>TITLE</b>" );
+    lblTitle->setText( QString( "<b>%1</b>" ).arg( _sTitle ) );
     center->addWidget( lblTitle );
     QLabel* lblContens = new QLabel;
-    lblContens->setText( "TESTSETESTSET" );
+    lblContens->setText( _sMsg );
     center->addWidget( lblContens );
 
     if( _eBtn != BTN_NONE )
@@ -157,8 +228,11 @@ void cToastNotification::SetToastUI()
 
     _btnClose = new QPushButton;
     _btnClose->setIcon( QIcon( ":/ICO/icon/cil-x.png" ) );
+    _btnClose->setIconSize( QSize( 20, 20 ) );
     _btnClose->setMaximumWidth( 30 );
     _btnClose->setMaximumHeight( 30 );
+    _btnClose->setStyleSheet( CLOSE_BTN_CSS );
+    connect( _btnClose, SIGNAL( clicked() ), this, SLOT( Close() ) );
 
     QFrame* frmRight = new QFrame;
     QVBoxLayout* lay = new QVBoxLayout;
@@ -173,16 +247,15 @@ void cToastNotification::SetToastUI()
     prepareToast();
 }
 
-void cToastNotification::SetToastBoxSize( int width, int height )
+void cToastNotification::SetToastBoxSize( QSize size )
 {
-    _nWidth = width;
-    _nHeight = height;
+    _nWidth = size.width();
+    _nHeight = size.height();
 }
 
-void cToastNotification::SetSpaceSize( int width, int height )
+void cToastNotification::SetSpaceSize( QSize size )
 {
-    _spaceSize.setWidth( width );
-    _spaceSize.setHeight( height );
+    _spaceSize = size;
 }
 
 void cToastNotification::prepareToast()
@@ -205,7 +278,6 @@ void cToastNotification::prepareToast()
 
     _wdgToastMsg->move( _wdgBase->width(), _wdgBase->height() - _wdgToastMsg->height() - _spaceSize.height() );
     _wdgToastMsg->show();
-
 }
 
 void cToastNotification::ShowToast()
@@ -224,4 +296,14 @@ void cToastNotification::ShowToast()
     connect( _animation, SIGNAL( finished() ), _animation, SLOT( deleteLater() ) );
 
     _animation->start( QAbstractAnimation::DeleteWhenStopped );
+}
+
+void cToastNotification::MoveToastUI( int x, int y )
+{
+    _wdgBase->move( x, y );
+}
+
+void cToastNotification::Close()
+{
+    delete this;
 }
--- QT/SharedQToastNotification.h
+++ QT/SharedQToastNotification.h
@@ -1,5 +1,5 @@
-#ifndef __HDR_SHARED_QT_UTILS__
-#define __HDR_SHARED_QT_UTILS__
+#ifndef __HDR_SHARED_QTOAST__
+#define __HDR_SHARED_QTOAST__
 
 #include "SharedBase.h"
 
@@ -12,6 +12,8 @@
 
 
 #ifdef USING_QT_LIBS
+
+class cToastNotification;
 
 typedef enum _tyToastIcon
 {
@@ -30,22 +32,47 @@ typedef enum _tyToastUsingBtn
     BTN_DOUBLE
 } TOAST_BUTTON;
 
+class cToastNotificationMgr : public QObject
+{
+    Q_OBJECT
+
+public:
+    cToastNotificationMgr( QWidget* parent = NULLPTR );
+    ~cToastNotificationMgr();
+
+    QSize                                 GetPosition();
+    void                                  MoveStack();
+    Q_INVOKABLE void                      ShowToastMsg( TOAST_ICON icon, TOAST_BUTTON btn, XString sTitle, XString sMsg, int nTimeOutSec = 10 );
+    Q_INVOKABLE void                      ShowToastMsg( QString sTitle, QString sMsg );
+
+private:
+    QWidget*                              _wdgParent;
+    QVector<cToastNotification*>          _vecNotification;
+
+    QSize                                 _ToastSize;
+    QSize                                 _SpaceSize;
+};
+
 class cToastNotification : public QObject
 {
     Q_OBJECT
 
 public:
     cToastNotification();
-    cToastNotification( QWidget* parent, TOAST_ICON icon, int nTimeOutSec, TOAST_BUTTON btn, XString sMsg );
+    cToastNotification( QWidget* parent, TOAST_ICON icon, TOAST_BUTTON btn, XString sTitle, XString sMsg, int nTimeOutSec = 10 );
     //cToastNotification( QWidget* parent, XString sMsg );
     ~cToastNotification();
 
     void                                  Init();
     void                                  SetToastUI();
-    void                                  SetToastBoxSize( int width, int height );
-    void                                  SetSpaceSize( int width, int height );
+    void                                  SetToastBoxSize( QSize size );
+    void                                  SetSpaceSize( QSize size );
 
     void                                  ShowToast();
+    void                                  MoveToastUI( int x, int y );
+
+public slots:
+    void                                  Close();
 
 private:
     void                                  prepareToast();
@@ -64,6 +91,7 @@ private:
     TOAST_ICON                            _eIcon;
     TOAST_BUTTON                          _eBtn;
 
+    XString                               _sTitle;
     XString                               _sMsg;
 
     QPushButton*                          _btnLeft;
