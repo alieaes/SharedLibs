4400dfd405facd4a7198e2080e9bf7054a7a0940
hogyun.yoon <hogyun.yoon@gytni.com>
2023-01-18 13:24:39 +0900

    추가 : FileDownload, 파일 다운로드 클래스 추가

---
+++ Network/SharedFileDownload.cpp
@@ -0,0 +1,156 @@
+﻿#include "stdafx.h"
+
+#include "Network/SharedFileDownload.h"
+
+#include "SharedBase.h"
+
+#include "Utils/SharedFile.h"
+#include "String/SharedXString.h"
+#include "String/SharedFormat.h"
+
+#include <Windows.h>
+#include <iostream>
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+namespace Shared
+{
+    namespace Network
+    {
+        cFileDownloader::cFileDownloader( XString sName )
+        {
+            Init();
+        }
+
+        cFileDownloader::~cFileDownloader()
+        {
+            if( _hInternet != NULLPTR )
+            {
+                InternetCloseHandle( _hInternet );
+                _hInternet = NULLPTR;
+            }
+        }
+
+        bool cFileDownloader::Init()
+        {
+            bool isSuccess = false;
+
+            do
+            {
+                _hInternet = InternetOpen( _sName, INTERNET_OPEN_TYPE_DIRECT, NULLPTR, NULLPTR, 0 );
+
+                if( _hInternet == NULLPTR )
+                    CONSOLEP( "InternetOpen Failed.." );
+
+                isSuccess = true;
+                _isInit = true;
+            }
+            while( false );
+            
+            return isSuccess;
+        }
+
+        bool cFileDownloader::DownloadFile( const XString& sUrl, const XString& sSaveFileFullPath )
+        {
+            bool isSuccess = false;
+            HINTERNET hOpenUrl = NULLPTR;
+            do
+            {
+                if( _hInternet == NULLPTR )
+                    break;
+
+                if( sUrl.IsEmpty() == true || sSaveFileFullPath.IsEmpty() == true )
+                    break;
+
+                XString sFilePath = File::FindFilePath( sSaveFileFullPath );
+
+                if( File::IsExistDir( sFilePath ) == false )
+                {
+                    if( File::MakeDir( sFilePath ) == false )
+                        break;
+                }
+
+                hOpenUrl = InternetOpenUrl( _hInternet, sUrl, nullptr, 0, INTERNET_FLAG_RELOAD, 0 );
+
+                if( hOpenUrl == NULLPTR )
+                    break;
+
+                DWORD dwStatus = 0;
+                DWORD dwStatusSize = sizeof( dwStatus );
+
+                if( HttpQueryInfo( hOpenUrl, HTTP_QUERY_STATUS_CODE | HTTP_QUERY_FLAG_NUMBER, &dwStatus, &dwStatusSize, NULLPTR ) == FALSE )
+                {
+                    //CONSOLEP( "HttpQueryInfo failed.. {}", ::GetLastError() );
+                    break;
+                }
+
+                if( sizeof( dwStatus ) != dwStatusSize )
+                {
+                    //CONSOLEP( "statusSize unMatch" );
+                    break;
+                }
+
+                if( dwStatus != HTTP_STATUS_OK )
+                {
+                    //CONSOLEP( "HttpQueryInfo Error {}", dwStatus );
+                    break;
+                }
+
+                const int nMaxBufferSize = 2048;
+                char buffer[ nMaxBufferSize ] = {};
+
+                HANDLE hFile = CreateFile( sSaveFileFullPath, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL );
+                if( hFile == INVALID_HANDLE_VALUE )
+                    break;
+
+                DWORD dwReadBytes = 0;
+                DWORD dwWrite = 0;
+                bool bDone = true;
+
+                do
+                {
+                    DWORD dwAvailable = 0;
+                    if( InternetQueryDataAvailable( hOpenUrl, &dwAvailable, 0, 0 ) == FALSE )
+                    {
+                        bDone = false;
+                        break;
+                    }
+
+                    if( dwAvailable > nMaxBufferSize )
+                        dwAvailable = nMaxBufferSize;
+
+                    if( InternetReadFile( hOpenUrl, buffer, dwAvailable, &dwReadBytes ) == FALSE )
+                    {
+                        bDone = false;
+                        break;
+                    }
+
+                    if( dwReadBytes > 0 )
+                    {
+                        WriteFile( hFile, buffer, dwReadBytes, &dwWrite, NULL );
+                        memset( buffer, 0x00, dwReadBytes );
+                    }
+
+                } while( dwReadBytes != 0 );
+
+                if( hFile != NULLPTR )
+                    CloseHandle( hFile );
+
+                isSuccess = bDone;
+
+            } while( false );
+
+            if( hOpenUrl != NULLPTR )
+                InternetCloseHandle( hOpenUrl );
+
+            return isSuccess;
+        }
+
+        bool cFileDownloader::DownloadFile( const XString& sUrl, const XString& sSaveFilePath, const XString& sSaveFileName )
+        {
+            return DownloadFile( sUrl, Format::Format( "{}\\{}", sSaveFilePath, sSaveFileName ) );
+        }
+    }
+}
---
+++ Network/SharedFileDownload.h
@@ -0,0 +1,40 @@
+#ifndef __HDR_SHARED_FILE_DOWNLOADER__
+#define __HDR_SHARED_FILE_DOWNLOADER__
+
+#include "SharedBase.h"
+
+#include <string>
+
+#include "Singleton.h"
+#include <wininet.h>
+
+#pragma comment(lib, "Wininet.lib")
+
+#ifdef USING_QT_LIBS
+#include <qstring.h>
+#endif // USING_QTLIB
+
+class XString;
+
+namespace Shared
+{
+    namespace Network
+    {
+        class cFileDownloader
+        {
+        public:
+            cFileDownloader( XString sName );
+            ~cFileDownloader();
+
+            bool                  Init();
+            bool                  DownloadFile( const XString& sUrl, const XString& sSaveFileFullPath );
+            bool                  DownloadFile( const XString& sUrl, const XString& sSaveFilePath, const XString& sSaveFileName );
+        private:
+            XString               _sName;
+            bool                  _isInit = false;
+            HINTERNET             _hInternet = NULLPTR;
+        };
+    }
+}
+
+#endif
