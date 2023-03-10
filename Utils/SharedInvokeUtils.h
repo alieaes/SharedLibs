e73c2a61fc5160458b1dafc605f56496a61bc4bc
hogyun.yoon <hogyun.yoon@gytni.com>
2023-03-10 15:51:50 +0900

    추가 : InvokeUtils, 함수 실행을 위한 대리 함수 추가

---
+++ Utils/SharedInvokeUtils.h
@@ -0,0 +1,38 @@
+﻿#ifndef __HDR_SHARED_INVOKE_UTILS__
+#define __HDR_SHARED_INVOKE_UTILS__
+
+#include "SharedBase.h"
+
+class BaseInvoker
+{
+public:
+    virtual void Invoke() = 0;
+};
+
+// Qt에서 실행되어야 하는 함수의 경우 예외 발생함. Wrapper함수로 invokeMethod 호출이 필요함.
+template<typename Ret, typename... Args>
+class cSInvoke : public BaseInvoker
+{
+public:
+    cSInvoke( std::function<Ret( Args... )> f, Args... args )
+    : func( std::move( f ) ), params( args... )
+    {
+        
+    }
+
+    Ret Invoke()
+    {
+        return std::apply( func, params );
+    }
+
+    void InvokeInThread()
+    {
+        auto f = std::async( std::launch::async, [this]() {return std::apply( func, params ); } );
+    }
+
+private:
+    std::function<Ret( Args... )> func;
+    std::tuple<Args...>           params;
+};
+
+#endif
