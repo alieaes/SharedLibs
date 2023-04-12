f847391368f2497a21d30b5a42cd62a5f0cefad8
hogyun.yoon <hogyun.yoon@gytni.com>
2023-04-12 17:34:01 +0900

    추가 : Timer, 실행속도 측정을 위한 함수

---
+++ Utils/SharedTimer.cpp
@@ -0,0 +1,46 @@
+﻿#include "stdafx.h"
+
+#include "Utils/SharedTimeChecker.h"
+
+#include <Windows.h>
+
+#include "String/SharedXString.h"
+
+#ifdef USING_QTLIB
+#include <qdatetime.h>
+#endif
+
+#pragma warning(disable:4996)
+
+cTimeChecker::cTimeChecker()
+{
+}
+
+void cTimeChecker::Start()
+{
+    _startTime = clock();
+}
+
+void cTimeChecker::Start( XString sSeparator )
+{
+    _startTime = clock();
+
+    _sSeparator = sSeparator;
+    _bUseSeparator = true;
+}
+
+double cTimeChecker::CheckPoint()
+{
+    clock_t check = clock();
+
+    double ret = static_cast< double >( check - _startTime ) / CLOCKS_PER_SEC;
+    return ret;
+}
+
+double cTimeChecker::End()
+{
+    clock_t check = clock();
+
+    double ret = static_cast< double >( check - _startTime ) / CLOCKS_PER_SEC;
+    return ret;
+}
